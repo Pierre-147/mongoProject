101e5942710e6e75ff1c394afa4e48adbc299a9c
Corentin-Blondeau <60091868+Corentin-Blondeau@users.noreply.github.com>
2021-09-20 17:43:53 +0200

    Add files via upload

--- codeprojet.py
+++ codeprojet.py
@@ -1,7 +1,5 @@
-# -*- coding: utf-8 -*-
 """
 Created on Tue Sep 14 15:28:24 2021
-
 @author: blond
 """
 
@@ -33,72 +31,75 @@ if __name__ == "__main__":
     collection_paris = part1.get_paris()
     for station in collection_paris['stations']:
        
-        formated = format( station['name'],
-                                    geometry={type: "point", 'coodinates':[station['latitude'],station['longitude']]},
+        formated = Donnees_station.formatt( station['name'],
+                                    geometry={'type': "point", 'coodinates':[station['latitude'],station['longitude']]},
                                     ville="Paris",
                                     nb_place_total=station['extra']['slots'],
                                     nb_place_dispo=station['empty_slots'],
                                     nb_velo_dispo=station['free_bikes'],
-                                    date=time.time()
+                                    date=time.asctime(time.gmtime())
         )
 
-        collection_name.insert_many([formated])
+        collection_name.insert_one(formated)
+    print("Paris fini")
 
 
     #Import data from Rennes
 
-    collection_2 = dbname["Rennes"]
+    collection_name = dbname["Rennes"]
     
     collection_rennes = part1.get_rennes()
     for station in collection_rennes['stations']:
 
-        formated = format( station['name'],
-                                    geometry={type: "point", 'coodinates':[station['latitude'],station['longitude']]},
+        formated = Donnees_station.formatt( station['name'],
+                                    geometry={'type': "point", 'coodinates':[station['latitude'],station['longitude']]},
                                     ville="Rennes",
                                     nb_place_total=station['extra']['slots'],
                                     nb_place_dispo=station['empty_slots'],
                                     nb_velo_dispo=station['free_bikes'],
-                                    date=time.time()
+                                    date=time.asctime(time.gmtime())
         )
 
-        collection_name.insert_many([formated])
+        collection_name.insert_one(formated)
+    print("Rennes fini")
 
 
 
     #Import data From Lille
 
-    collection_2 = dbname["Lille"]
+    collection_name = dbname["Lille"]
     
     collection_lille = part1.get_lille()
     for station in collection_lille:
         
-        formated = format( station['fields']['nom'],
-                                    geometry={type: "point", 'coodinates':station['fields']['geo']},
+        formated = Donnees_station.formatt( station['fields']['nom'],
+                                    geometry={'type': "point", 'coodinates':station['fields']['geo']},
                                     ville="Lille",
-                                    nb_place_total=station['fields']['nbplacesdispo'] + station['fields']['nvelosdispo'],
+                                    nb_place_total=station['fields']['nbplacesdispo'] + station['fields']['nbvelosdispo'],
                                     nb_place_dispo=station['fields']['nbplacesdispo'],
-                                    nb_velo_dispo=station['fields']['nvelosdispo'],
-                                    date=time.time()
+                                    nb_velo_dispo=station['fields']['nbvelosdispo'],
+                                    date=time.asctime(time.gmtime())
         )
 
-        collection_name.insert_many([formated])
-        
+        collection_name.insert_one(formated)
+    print("Lille fini")
 
 
     #Import data From Lyon
 
-    collection_2 = dbname["Lyon"]
+    collection_name = dbname["Lyon"]
     
     collection_lyon = part1.get_lyon()
     for station in collection_lyon['stations']:
         
-        formated = format( station['name'],
-                                    geometry={type: "point", 'coodinates':[station['latitude'],station['longitude']]},
+        formated = Donnees_station.formatt( station['name'],
+                                    geometry={'type': "point", 'coodinates':[station['latitude'],station['longitude']]},
                                     ville="Lyon",
                                     nb_place_total=station['extra']['slots'],
                                     nb_place_dispo=station['empty_slots'],
                                     nb_velo_dispo=station['free_bikes'],
-                                    date=time.time()
+                                    date=time.asctime(time.gmtime())
         )
 
-        collection_name.insert_many([formated])
+        collection_name.insert_one(formated)
+    print("Lyon fini")
