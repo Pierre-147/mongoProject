d7fd79577952f9499304b4b5ea988c7cc62daf9b
Pierre <pierre.niclas@student.yncrea.fr>
2021-09-20 11:39:21 +0200

    accès aux stations des 4 villes (API Paris pas extra)

--- codeprojet.py
+++ codeprojet.py
@@ -13,7 +13,6 @@ def get_database():
 
     client = pymongo.MongoClient("mongodb+srv://python:python@database1.fv5bh.mongodb.net/Database1?retryWrites=true&w=majority", ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
 
-    
     return client['Vlib_Paris']
     
 
@@ -25,7 +24,29 @@ if __name__ == "__main__":
     collection_name = dbname["Paris"]
     
     collection_paris = part1.get_paris()
-    collection_name.insert_many(collection_paris)
+    for station in collection_paris:
+         collection_name.insert_many([station])
+
+    """   
+    collection_2 = dbname["Rennes"]
     
+    collection_rennes = part1.get_rennes()
+    for station in collection_rennes:
+        collection_2.insert_many([station])
+
+    collection_2 = dbname["Lille"]
     
+    collection_lille = part1.get_lille()
+    for station in collection_lille:
+        collection_2.insert_many([station])
     
+
+
+
+    collection_2 = dbname["Lyon"]
+    
+    collection_lyon = part1.get_lyon()
+    for station in collection_lyon:
+        collection_2.insert_many([station])
+
+   """
--- part1.py
+++ part1.py
@@ -3,6 +3,7 @@ import json
 
 
 def get_lille():
+    #fonction
     url="https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=3000&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
@@ -10,23 +11,25 @@ def get_lille():
 
 
 def get_lyon():
+
     url="https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json"
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
-    return response_json.get("records", [])
+    return response_json.get("values", [])
 
 def get_paris():
     #fonction mais changer de set de données
-    url="https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json"
+    url="https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=stationcode&refine.stationcode=16107"
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
 
-    return response_json.get("data", [])
+    return response_json.get("records", [])
 
 def get_rennes():
+    #fonctionne et dataset correct
     url="https://data.rennesmetropole.fr/explore/dataset/etat-des-stations-le-velo-star-en-temps-reel/download?format=json"
     response = requests.request("GET", url)
     response_json = json.loads(response.text.encode('utf8'))
     return response_json
 
-
+print(get_paris())
