9db866ed802850cab774b5739a952e9aa29f7770
Corentin-Blondeau <60091868+Corentin-Blondeau@users.noreply.github.com>
2021-09-20 12:02:36 +0200

    Add files via upload
    Le format des données station

---
+++ Donnees_station.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 11:45:31 2021
+
+@author: blond
+"""
+
+class Donnees_station:
+    def __init__(self):
+        self.nom=""
+        geometry: {
+            type: "Point",
+            coordinates: [0, 0]
+            }
+        self.ville=""
+        self.nb_place_total=0
+        self.nb_place_dispo=0
+        self.nb_velo_dispo=0
+        self.date=""
+    
