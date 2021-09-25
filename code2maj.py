0bea29b404d9947da8fc4aee255b59d8f7212560
Corentin-Blondeau <60091868+Corentin-Blondeau@users.noreply.github.com>
2021-09-25 17:36:02 +0200

    fonction de requêtage de la base MongoDB
    fonction de recherche
    fonction d'update
    fonction de suppression
    fonction de ratio

--- code2maj.py
+++ code2maj.py
@@ -11,30 +11,11 @@ import pprint
 import pymongo
 import ssl
 import time
-
 def get_database():
 
     client = pymongo.MongoClient("mongodb+srv://python:python@database1.fv5bh.mongodb.net/Database1?retryWrites=true&w=majority", ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
 
     return client['Stations_Velib']
-    
-def history_data():
-    # Get the database
-    dbname = get_database()
-    
-    # Move the data to collection 'historique'
-    dbname['stations_actuelles'].aggregate([
-        {
-            '$match': {}
-        }, {
-            '$out': 'historique'
-        }
-        ])
-    # Delete data  in 'station'
-    print('Les données ont été historisées')
-    dbname['stations_actuelles'].delete_many({})
-    # Put new data
-    codeprojet.maj_collection(dbname)
 
 def near(coordonnees):
     # Get the database
@@ -95,18 +76,112 @@ def ratio():
     for i in result:
         pprint.pprint(i)
 
-def admin_programme():
-    select=input("Selection de mode")
-    if select==1:#search
-        a=1
-    elif select==2:#update
-        a=2
+def selection():
+    
+    dbname = get_database()
+    fini=False
+    query=[]
+    while not fini:
+        print("Sélection selon quel critère :")
+        print("Ville : 1")
+        print("Nom de station : 2")
+        print("Coordonnées : 3")
+        print("Nombre de places total : 4")
+        print("Nombre de vélos disponible : 5")
+        print("Nombre de places disponibles : 6")
+        select='0'
+        while select>'6' or select<'1':
+            select=input()
+        if select=='1':
+            recherche=input('nom de la ville : ')
+            print(recherche)
+            query.append(
+            {
+                '$match': {
+                    'villle': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        if select=='2':
+            recherche=input('nom de la station : ')
+            print(recherche)
+            query.append(
+            {
+                '$match': {
+                    'nom': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        if select=='3':
+            longitude=input('Longitude : ')
+            lattitude=input('Lattitude : ')
+            print(longitude,lattitude) #############################
+            query.append(
+            {
+                '$match': {
+                    'villle': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        if select=='4':
+            recherche=int(input('Nombre de places total : '))
+            print(recherche)
+            query.append(
+            {
+                '$match': {
+                    'nb_place_total': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        if select=='5':
+            recherche=int(input('Nombre de vélos disponible : '))
+            print(recherche)
+            query.append(
+            {
+                '$match': {
+                    'nb_velo_dispo': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        if select=='6':
+            recherche=int(input('Nombre de places disponible : '))
+            print(recherche)
+            query.append(
+            {
+                '$match': {
+                    'nb_place_dispo': {
+                        '$eq': recherche
+                    }
+                }
+            })
+        
+        tempo=input("Avez-vous fini la sélection des critères ? O/N : ").upper()
+        while tempo!='O' and tempo!='N':
+            tempo=input()
+        if tempo=='O':
+            fini=True
+    query.append({
+            '$unset': ['_id']
+        })
+    result = dbname['historique'].aggregate(query)
+    print("Résultats de la recherche :")
+    for i in result:
+        pprint.pprint(i)
+
+
+    
 
 if __name__ == "__main__":    
-    history_data()
-    
-     # coordonnees=[48,2]
-     # near(coordonnees)
+    selection()
+    print('fin')
+    prout=input()
+    # coordonnees=[48,2]
+    # near(coordonnees)
     #ratio()
     
 
