d23837b4fbc7940531d6464527d345e3668c0b12
Pierre <pierre.niclas@student.yncrea.fr>
2021-09-26 14:39:58 +0200

    new functions

--- recherche_regexp.py
+++ recherche_regexp.py
@@ -4,3 +4,10 @@ def search(name, collection):
     query = { "nom": { "$regex": name } }
     res = collection.find(query)
     return res
+
+def search(name, collection):
+    query = { "nom": { "$regex": name } }
+    collection.delet_many(query)
+
+def update(name, item, newvalue, collection):
+    collection.update_one({"nom": name}, {"$set": { item: newvalue}})
