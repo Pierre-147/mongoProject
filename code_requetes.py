c4474133cd2a23db55018afa340da3283aba60bf
Pierre <pierre.niclas@student.yncrea.fr>
2021-09-27 12:05:02 +0200

    commentaires sur code_requetes.py

--- code_requetes.py
+++ code_requetes.py
@@ -1,17 +1,14 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 20 14:48:28 2021
-
-@author: blond
-"""
-
-import pprint
-import pymongo
-import ssl
+import pprint   #module pour la présentation des resultats
+import pymongo  #module pour utiliseer mongodb
+import ssl      #module pour se connecter au serveurs mongodb
 
 def get_database():
     client = pymongo.MongoClient("mongodb+srv://python:python@database1.fv5bh.mongodb.net/Database1?retryWrites=true&w=majority", ssl=True,ssl_cert_reqs=ssl.CERT_NONE)
     return client['Stations_Velib']
+
+# =============================================================================
+# near cherche les trois stations les plus proches des coordonées indiquez et renvoie
+# leur nom, ville, nombre de vélo et de places disponibles
 
 def near():
     print("Importer vos coordonnées : ")
@@ -42,6 +39,9 @@ def near():
     for i in liste_near:
         pprint.pprint(i)
 
+# =============================================================================
+# recherche renvoie toute les stations dont le nom contient les  carachteres indiquez
+# sans se soucier de la capitalisation
     
 def recherche(name=''):
     if name=='':
@@ -53,6 +53,8 @@ def recherche(name=''):
     for i in result:
         pprint.pprint(i)
 
+# =============================================================================
+# update permet de modifier un des elements d'une station dont il faut indiquer le nom entier
 
 def update():
     recherche()
@@ -66,6 +68,9 @@ def update():
     recherche(name)
 
 
+# =============================================================================
+# supprimer permet de supprimer toutes les information d'une station dont on indique le nom exacte
+
 def supprimer():
     recherche()
     name=input("Donner le nom complet de la station : ")
@@ -74,6 +79,9 @@ def supprimer():
     dbname['stations_actuelles'].delete_many(query)
     print("Station supprimée")
 
+# =============================================================================
+# supprime_zones permet de selectioner toutes stations dans un rectangle dont on fournit les coins
+# et les supprime toutes
 
 def supprimer_zone():
     coordonnees=[]
@@ -95,6 +103,11 @@ def supprimer_zone():
     dbname = get_database()
     dbname['stations_actuelles'].delete_many(query)
     print("Stations de la zone supprimées")
+
+# =============================================================================
+# find_overused_station selectione toutes les données de l'historique provenant d'un jour de la semaine
+# et ayant ete obtenue entre 18h et 19h, calcul le ratio de velos disponible par rapport à la taille de la station
+# puis renvoie celles dont le ratio est inférieur a 0.2
 
 def find_overused_station():
     query=[
